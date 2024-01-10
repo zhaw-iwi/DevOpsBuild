61f8c593c4418cbd76241f60ff3e5c60bf971dda
mosazhaw <adrian.moser@zhaw.ch>
2024-01-10 11:33:39 +0100

    pdfbox added

--- app/src/main/java/test/App.java
+++ app/src/main/java/test/App.java
@@ -3,6 +3,16 @@
  */
 package test;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.font.Standard14Fonts;
+
 import com.indvd00m.ascii.render.Render;
 import com.indvd00m.ascii.render.api.ICanvas;
 import com.indvd00m.ascii.render.api.IContextBuilder;
@@ -14,7 +24,7 @@ public class App {
         return "Hello World!";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println(new App().getGreeting());
 
         // Ascii Render Example
@@ -26,5 +36,20 @@ public class App {
         String s = canvas.getText();
         System.out.println(s);
 
+        // PDF Box
+        PDDocument helloPdf = new PDDocument();
+        PDPage page = new PDPage(PDRectangle.A4);
+        helloPdf.addPage(page);
+
+        PDPageContentStream contentStream = new PDPageContentStream(helloPdf, page);
+        contentStream.beginText();
+        contentStream.setFont(new PDType1Font(Standard14Fonts.FontName.HELVETICA), 36);
+        contentStream.newLineAtOffset(5, 400);
+        contentStream.showText("Hello DevOps!!!");
+        contentStream.endText();
+        contentStream.close();
+
+        helloPdf.save(new File("simple.pdf"));
+        helloPdf.close();
     }
 }
